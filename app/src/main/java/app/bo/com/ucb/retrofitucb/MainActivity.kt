d02e9bc750169fc870275ee4afc33b5351d7d0d1
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2021-03-19 18:13:32 -0400

    added retrofit in MainActivity

--- app/src/main/java/app/bo/com/ucb/retrofitucb/MainActivity.kt
+++ app/src/main/java/app/bo/com/ucb/retrofitucb/MainActivity.kt
@@ -2,10 +2,32 @@ package app.bo.com.ucb.retrofitucb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val restApiAdapter = RestApiAdapter()
+        val endPoint = restApiAdapter.conectionApi()
+
+        val postResponseCall = endPoint.getAllPost()
+
+        postResponseCall.enqueue( object : Callback<List<Post>> {
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+                t?.printStackTrace()
+            }
+
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                val posts = response.body()
+                Log.d("RESP POST", Gson().toJson(posts))
+            }
+
+        })
     }
 }
